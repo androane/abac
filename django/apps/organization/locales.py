4235930b245413866c96aaf9fba712e5b587a6ae
Andi Zamfir <mihai.zamfir90@proton.me>
2024-04-09 10:39:11 +0300

    invoice details

--- django/apps/organization/locales.py
+++ django/apps/organization/locales.py
@@ -18,3 +18,18 @@ CATEGORY_TRANSLATIONS = {
     "Accounting": "Contabilitate",
     "Human Resources": "Resurse Umane",
 }
+
+MONTH_TRANSLATIONS = {
+    1: "Ianuarie",
+    2: "Februarie",
+    3: "Martie",
+    4: "Aprilie",
+    5: "Mai",
+    6: "Iunie",
+    7: "Iulie",
+    8: "August",
+    9: "Septembrie",
+    10: "Octombrie",
+    11: "Noiembrie",
+    12: "Decembrie",
+}
--- django/apps/organization/views.py
+++ django/apps/organization/views.py
@@ -1,26 +1,56 @@
 # -*- coding: utf-8 -*-
 from http.client import HTTPResponse
 
+from django.core.exceptions import PermissionDenied
+
+from core.exceptions import PermissionException
 from core.renderers import render_to_pdf
+from organization.locales import CATEGORY_TRANSLATIONS, MONTH_TRANSLATIONS
 from organization.models.invoice import Invoice
-from report.services.denormalize_models_service import get_flattened_report_data
+from organization.services.client.client_activity_service import (
+    get_client_activities,
+    get_client_solutions,
+)
+from user.permissions import UserPermissionsEnum, validate_has_permission
 
 
 def download_invoice_details(request) -> HTTPResponse:
     user = request.user
+
+    if not user.is_authenticated:
+        raise PermissionDenied
+
+    try:
+        validate_has_permission(
+            user, UserPermissionsEnum.HAS_CLIENT_INVOICE_ACCESS.value
+        )
+    except PermissionException:
+        raise PermissionDenied
+
     org = user.organization
 
     invoice = Invoice.objects.get(
         uuid=request.GET.get("invoice_uuid"), client__organization=org
     )
 
-    df = get_flattened_report_data(
-        org, invoice.year, invoice.month, client_uuids=[invoice.client.uuid]
-    )
-    print(df)
-    response = render_to_pdf("invoice_details_template.html", {})
+    client = invoice.client
 
-    filename = f"detalii_factura_{invoice.year}_{invoice.month}.pdf"
+    client_solutions = get_client_solutions(user, client, invoice.month, invoice.year)
+    client_activities = get_client_activities(
+        user, client, invoice.month, invoice.year
+    ).filter(is_executed=True)
+
+    context = {
+        "month": MONTH_TRANSLATIONS[invoice.month],
+        "year": invoice.year,
+        "CATEGORY_TRANSLATIONS": CATEGORY_TRANSLATIONS,
+        "client_solutions": client_solutions,
+        "client_activities": client_activities,
+    }
+
+    response = render_to_pdf("invoice_details_template.html", context)
+
+    filename = f"detalii_factura_{invoice.month}_{invoice.year}.pdf"
 
     content = f"attachment; filename={filename}"
     response["Content-Disposition"] = content
